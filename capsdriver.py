ed7ddd9b4125ad3a4726d3de5260876a010a9e88
ngetty <ngetty@hawk.iit.edu>
2018-05-07 14:35:18 -0500

    capsnet scripts, run using capsdriver

---
+++ capsdriver.py
@@ -0,0 +1,221 @@
+import os
+import numpy as np
+import scipy
+from sklearn.model_selection import train_test_split
+import h5py
+from sklearn.cross_validation import StratifiedKFold
+import capsnet
+import mri
+import nibabel as nib
+import fnmatch
+import pandas as pd
+import argparse
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, Input
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import callbacks, optimizers
+
+
+def params():
+    # setting the hyper parameters
+    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
+    parser.add_argument('--epochs', default=60, type=int)
+    parser.add_argument('--batch_size', default=100, type=int)
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.9, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    parser.add_argument('--lam_recon', default=0.392, type=float,
+                        help="The coefficient for the loss of decoder")
+    parser.add_argument('-r', '--routings', default=3, type=int,
+                        help="Number of iterations used in routing algorithm. should > 0")
+    parser.add_argument('--shift_fraction', default=0.1, type=float,
+                        help="Fraction of pixels to shift at most in each direction.")
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    parser.add_argument('--cv', action='store_true',
+                        help="Use cross validation")
+    parser.add_argument('--cnn', action='store_true',
+                        help="Use cnn model")
+    parser.add_argument('--save_dir', default='./result')
+    parser.add_argument('--data', default='tumor')
+    parser.add_argument('-t', '--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+    parser.add_argument('--digit', default=5, type=int,
+                        help="Digit to manipulate")
+    parser.add_argument('-w', '--weights', default=None,
+                        help="The path of the saved weights. Should be specified when testing")
+    args = parser.parse_args()
+    print(args)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    return args
+
+
+def load_tumor():
+    X = []
+    y = []
+
+    path = 'data/tumor_data'
+
+    for file in os.listdir(path):
+        f = h5py.File(os.path.join(path, file), 'r')
+        label = int(f.get('cjdata/label')[0][0])
+        img = np.array(f.get('cjdata/image'))
+        img = scipy.misc.imresize(img, (64, 64))
+        X.append(img)
+        l = [0, 0, 0]
+        l[label - 1] = 1
+        y.append(l)
+
+    X = np.stack(X).reshape(len(X), 64, 64, 1).astype('float32') / 255
+    y = np.stack(y)
+
+    tts_split = train_test_split(
+        X, y, range(y.shape[0]), test_size=0.3, random_state=0, stratify=y#np.argmax(y, axis=1)
+    )
+    
+    x_train, x_test, y_train, y_test, train_idx, test_idx = tts_split
+    
+    x_train = x_train.reshape(x_train.shape[0], 64, 64, 1)
+    x_test = x_test.reshape(x_test.shape[0], 64, 64, 1)
+
+    return x_train, x_test[:-300], y_train, y_test[:-300], X, y, x_test[-300:], y_test[-300:]
+
+
+def load_tbi():
+     # load data
+    imgs = mri.load_imgs()
+
+    match_df = mri.load_target('CT_Intracraniallesion_FIN')
+
+    X, y = mri.match_image_ids(imgs, match_df)
+
+    x_train, x_test, y_train, y_test = mri.get_split(X, y, 2)
+
+    return x_train, x_test, y_train, y_test
+
+
+def load_control():
+    # fn = '/vol/ml/ngetty/control/ABIDE/'
+    fn = '/Users/ngetty/Downloads/control/ABIDE/'
+    ids = []
+    X = []
+    for root, dirnames, filenames in os.walk('src'):
+        for filename in fnmatch.filter(filenames, '*.nii'):
+            fn = os.path.join(root, filename)
+            img = nib.load(fn)
+            img_data = img.get_data()
+            ids.append(filename[7:12])
+            X.append(img_data)
+
+    X = np.stack(X).reshape(len(X), 170, 256, 256, 1)
+
+    infile = 'data/control.csv'
+    df = pd.read_csv(infile, usecols=['Subject', 'Age'])
+    df.set_index("Subject", drop=True, inplace=True)
+    age_dict = df.to_dict(orient="index")
+
+    y = [age_dict[id] for id in ids]
+
+    tts_split = train_test_split(
+        X, y, range(y.shape[0]), test_size=0.2, random_state=0, stratify= y#np.argmax(y, axis=1)
+    )
+
+    x_train, x_test, y_train, y_test, train_idx, test_idx = tts_split
+
+    x_train = x_train.reshape(x_train.shape[0], 170, 256, 256, 1) #.astype('float32') / 255
+    x_test = x_test.reshape(x_test.shape[0], 170, 256, 256, 1) #.astype('float32') / 255
+
+    return x_train, x_test, y_train, y_test
+
+
+def cnn_model():
+    img_input = Input(shape=(64, 64, 1), name='input')
+
+    # --- block 1 ---
+    x = Conv2D(64, (5, 5), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    x = Conv2D(64, (5, 5), activation='relu', padding='same', name='block2_conv1')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    x = Flatten(name='flatten')(x)
+    x = Dense(800, activation='relu', name='fc_1')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    x = Dense(800, activation='relu', name='fc_2')(x)
+    x = BatchNormalization()(x)
+    #x = Dropout(0.8)(x)
+    pred = Dense(3, activation='sigmoid', name='pred')(x)
+    model = Model(img_input, pred, name='mri_regressor')
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+def main():
+    args = params()
+    d = 3
+    if args.data == 'tumor':
+        x_train, x_test, y_train, y_test, X, y, x_hold, y_hold = load_tumor()
+        d = 2
+    elif args.data == 'tbi':
+        x_train, x_test, y_train, y_test = load_tbi()
+    elif args.data == 'control':
+        x_train, x_test, y_train, y_test = load_control()
+
+    if args.cnn:
+        model = cnn_model()
+    else:
+        model, eval_model, manipulate_model = capsnet.CapsNet(input_shape=x_train.shape[1:],
+                                                              n_class=len(np.unique(np.argmax(y_train, 1))),
+                                                              routings=args.routings, d=d)
+        # compile the model
+        model.compile(optimizer=optimizers.Adam(lr=args.lr),
+                      loss=[capsnet.margin_loss, 'mse'],
+                      loss_weights=[1., args.lam_recon],
+                      metrics={'capsnet': 'accuracy'})
+
+    lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
+    es = callbacks.EarlyStopping(min_delta=0.001, patience=10)
+
+    if args.cv:
+        skf = StratifiedKFold(np.argmax(y, axis=1), n_folds=5, shuffle=True)
+
+        for i, (tr, te) in enumerate(skf):
+            model = cnn_model()
+            model.fit(X[tr], y[tr],
+                      batch_size=100,
+                      epochs=60,
+                      verbose=1,
+                      callbacks=[lr_decay, es],
+                      validation_data=(X[te], y[te]),
+                      class_weight='auto')
+    else:
+        if args.cnn:
+            model.fit(x_train, y_train,
+                      batch_size=50,
+                      epochs=60,
+                      verbose=1,
+                      callbacks=[lr_decay, es],
+                      validation_data=(x_test, y_test),
+                      class_weight='auto')
+            print model.evaluate(x_hold, y_hold)
+        else:
+            model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
+                      validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[lr_decay])
+
+            print model.evaluate([x_hold, y_hold], [y_hold, x_hold])
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
---
+++ capsnet.py
@@ -0,0 +1,105 @@
+"""
+Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Keras`
+"""
+
+import numpy as np
+from keras import layers, models, optimizers
+from keras import backend as K
+import matplotlib.pyplot as plt
+from utils import combine_images
+from PIL import Image
+from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
+from keras.preprocessing.image import ImageDataGenerator
+from keras import callbacks
+K.set_image_data_format('channels_last')
+
+
+def CapsNet(input_shape, n_class, routings, d):
+    """
+    A Capsule Network on MNIST.
+    :param input_shape: data shape, 3d, [width, height, channels]
+    :param n_class: number of classes
+    :param routings: number of routing iterations
+    :return: Two Keras Models, the first one used for training, and the second one for evaluation.
+            `eval_model` can also be used for training.
+    """
+    x = layers.Input(shape=input_shape)
+
+    # Layer 1: Just a conventional Conv2D layer
+    if d == 2:
+        conv1 = layers.Conv2D(filters=64, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+    else:
+        conv1 = layers.Conv3D(filters=64, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+
+    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid', d=d)
+
+    # Layer 3: Capsule layer. Routing algorithm works here.
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings,
+                             name='digitcaps')(primarycaps)
+
+    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
+    # If using tensorflow, this will not be necessary. :)
+    out_caps = Length(name='capsnet')(digitcaps)
+
+    # Decoder network.
+    y = layers.Input(shape=(n_class,))
+    masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
+    masked = Mask()(digitcaps)  # Mask using the capsule with maximal length. For prediction
+
+    # Shared Decoder model in training and prediction
+    decoder = models.Sequential(name='decoder')
+    decoder.add(layers.Dense(512, activation='relu', input_dim=16 * n_class))
+    decoder.add(layers.Dense(1024, activation='relu'))
+    decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
+    decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
+
+    # Models for training and evaluation (prediction)
+    train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
+    eval_model = models.Model(x, [out_caps, decoder(masked)])
+
+    # manipulate model
+    noise = layers.Input(shape=(n_class, 16))
+    noised_digitcaps = layers.Add()([digitcaps, noise])
+    masked_noised_y = Mask()([noised_digitcaps, y])
+    manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
+
+    return train_model, eval_model, manipulate_model
+
+
+def margin_loss(y_true, y_pred):
+    """
+    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
+    :param y_true: [None, n_classes]
+    :param y_pred: [None, num_capsule]
+    :return: a scalar loss value.
+    """
+    L = y_true * K.square(K.maximum(0., 0.9 - y_pred)) + \
+        0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
+
+    return K.mean(K.sum(L, 1))
+
+
+def manipulate_latent(model, data, args):
+    print('-' * 30 + 'Begin: manipulate' + '-' * 30)
+    x_test, y_test = data
+    index = np.argmax(y_test, 1) == args.digit
+    number = np.random.randint(low=0, high=sum(index) - 1)
+    x, y = x_test[index][number], y_test[index][number]
+    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+    noise = np.zeros([1, 10, 16])
+    x_recons = []
+    for dim in range(16):
+        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+            tmp = np.copy(noise)
+            tmp[:, :, dim] = r
+            x_recon = model.predict([x, y, tmp])
+            x_recons.append(x_recon)
+
+    x_recons = np.concatenate(x_recons)
+
+    img = combine_images(x_recons, height=16)
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+    print('-' * 30 + 'End: manipulate' + '-' * 30)
