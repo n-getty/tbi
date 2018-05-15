29bbbbca47c38cd6bc0bc58637e1a7a7f289cbae
ngetty <ngetty@hawk.iit.edu>
2018-05-15 03:25:44 -0500

    testing capsnet regression model

--- capsnet.py
+++ capsnet.py
@@ -46,8 +46,8 @@ def CapsNet(input_shape, n_class, routings, d):
     out_caps = Length(name='capsnet')(digitcaps)
 
     out_reg = Length(name='capsreg')(digitcaps)
-    reg_pred = layers.Dense(1024, activation='relu')(out_reg)
-    reg_pred = layers.Dense(1, activation='linear', name='reg')(reg_pred)
+    #reg_pred = layers.Dense(1024, activation='relu')(out_reg)
+    reg_pred = layers.Dense(1, activation='linear', name='reg')(out_reg)
 
     # Decoder network.
     y = layers.Input(shape=(n_class,))
