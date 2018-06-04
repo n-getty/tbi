fb26c961ee11e6913dca992fb1a86bce0aa89184
ngetty <ngetty@hawk.iit.edu>
2018-06-04 14:54:16 -0500

    fcon preprocessing

--- comb_csv.py
+++ comb_csv.py
@@ -10,6 +10,12 @@ for root, dirnames, filenames in os.walk(fn):
     for filename in fnmatch.filter(filenames, '*.txt'):
         file = os.path.join(root, filename)
         df = pd.read_csv(file, usecols=[0,2,3], names=['ID', 'Age', 'Sex'], delimiter='\t')
+        if df.Age[0] in ['m', 'f']:
+            df = pd.read_csv(file, usecols=[0, 3, 2], names=['ID', 'Age', 'Sex'], delimiter='\t')
+        elif df.Age[0] == 'y':
+            print file
+            break
+            
         demos.append(df)
 
 demos = pd.concat(demos)
